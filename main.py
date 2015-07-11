69bc5964a6952fef130430ba3d4fd2eca9cc9c4e
Cong <congusbongus@gmail.com>
2015-07-11 17:44:44 +1000

    Basic skeleton

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    turn = 0
+    while True:
+        print "It is turn", turn
+        raw_input("Press enter to go to next turn")
+        turn += 1
+
+
+if __name__ == "__main__":
+    main()
