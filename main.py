6fbec9f208461120eb758713b598253477fd0a59
Michael Hoffman <mhoffman@digitaltomato.com.au>
2015-07-11 18:06:07 +1000

    Added an end condition

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ from adventurer import Adventurer
 
 
 def main():
-    turn = 0
+    turnsPerDay = 5
+    totalNumberOfDays = 2
+    day = 1
+    turn = 1
     adventurers = []
 
     # Add some adventurers
     adventurers.append(Adventurer("Crono"))
     adventurers.append(Adventurer("Red"))
 
-    while True:
-        print "It is turn", turn
+    while day <= totalNumberOfDays:
+        print "Day:", day, ", Turn:", turn
         print "The adventurers are:", adventurers
-        raw_input("Press enter to go to next turn")
+        inp = raw_input("Press enter to go to next turn")
+        print inp
         turn += 1
+        if (turn > turnsPerDay):
+            turn = 1
+            day += 1
+
+    print "Shit bro.... Your village is burning, your maidens are raped and your children are murdered"
+
 
 
 if __name__ == "__main__":
